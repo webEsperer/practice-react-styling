44f6b4fb2b8a266ecc820f9d0513654225be8e04
devmentor.pl <55095257+devmentor-pl@users.noreply.github.com>
2022-11-17 15:43:56 +0100

    update to React v.18

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 // ./src/app.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 
 
-ReactDOM.render(
-  <App />, document.querySelector('#root')
-);
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
